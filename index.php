ebef7d51964828033a564aae85aa6abb59ae0407
DanteBoute <danteboute1@outlook.com>
2024-01-09 15:07:07 +0100

    Finish ex. 1

--- index.php
+++ index.php
@@ -12,6 +12,8 @@ Calculate how much of the total price is tax (fruit goes at 6%, wine is 21%)
 Next, do the same with classes. What style do you prefer? Do you notice any difference in time needed to code, structure or readability? From now on, if nothing is stated specifically, it's recommended to use classes.
 */
 
+
+// -----------------------------------------------
 $bananasQuantity = 6;
 $bananasPrice = 1;
 
@@ -36,3 +38,48 @@ $totalPriceOfFruitBasket = $totalApplePrice + $totalBananaPrice + $totalWineBott
 
 echo "The total price of a fruit basket is € $totalPriceOfFruitBasket.<br>";
 echo "The total tax on a fruit basket is € $totalTax.";
+
+// -----------------------------------------------
+
+class BasketItems {
+    private $name;
+    private $price;
+    private $amount;
+    private $type;
+function __construct(string $name, float $price, float $amount, string $type) {
+    $this->name = $name;
+    $this->price = $price;
+    $this->amount = $amount;
+    $this->type = $type;
+}
+function calcPrice(): float {
+    $totalPrice = $this->price * $this->amount;
+    return $totalPrice;
+}
+function calcFruitTax(): float {
+    return ($this->price * $this->amount)*0.06;
+}
+function calcWineTax(): float {
+    return ($this->price * $this->amount)*0.21;
+}
+}
+$banana = new BasketItems("banana", 1, 6, "fruit");
+$apple = new BasketItems("apple", 1.5, 3, "fruit");
+$bottleOfWine = new BasketItems("bottleOfWine", 10, 2, "drink");
+
+
+$totalBananaPrice = $banana->calcPrice();
+$totalApplePrice = $apple->calcPrice();
+$totalWinePrice = $bottleOfWine->calcPrice();
+
+$appleTax = $apple->calcFruitTax();
+$bananaTax = $banana->calcFruitTax();
+$wineTax = $bottleOfWine->calcWineTax();
+
+
+$totalPrice = $totalBananaPrice + $totalApplePrice + $totalWinePrice;
+echo "<br>The total price of a fruit basket is: €$totalPrice.";
+
+$totalTax = $wineTax + $bananaTax + $appleTax;
+
+echo "<br>The total tax on a fruit basket is: €$totalTax.";
